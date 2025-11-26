1cf459e9135019c1f1d21f0e6bf3210d43ce4b39
Motheo-365 <motheomorena6@gmail.com>
2025-11-26 21:39:59 +0200

    changed comic to stats.html with a link to the comic

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -37,7 +37,7 @@
       ></video>
       <div class="links">
         <a href="#top" title="Back to Top of Projects">← Back</a>
-        <a href="/Comic/comic.html" target="_blank" title="View the Comic">View It Here</a>
+        <a href="Comic\stats\stats.html" target="_blank" title="View the Comic">View It Here</a>
       </div>
     </section>
 
